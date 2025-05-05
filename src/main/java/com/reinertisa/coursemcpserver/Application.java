f2baab14e056d861b3f70b791b6de62fd658c785
Isa Reinert <reinert.isa@gmail.com>
2025-05-05 00:12:18 -0700

    feat(ai): initialize mcp project

---
+++ src/main/java/com/reinertisa/coursemcpserver/Application.java
@@ -0,0 +1,13 @@
+package com.reinertisa.coursemcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
